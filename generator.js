a3b504d42b62db10eece16e57cfe8e69ba7af73d
Davis Engeler <davisengeler+github@gmail.com>
2017-08-09 20:46:11 -0500

    Add basic comments to generator.js

--- generator.js
+++ generator.js
@@ -1,4 +1,11 @@
-'use strict';
+// This code creates and validates random, ideal generators based on the iterator.
+// 
+// Inputs...
+//  - instanceName: Identifyer for this logical instance. 
+//	- iterator: The current iteration (how many IDs have been used).
+//
+// Outputs...
+//  - Validated random generator within ideal range for required capacity. 
 
 function gcd(a,b) {
     a = Math.abs(a);
@@ -12,21 +19,21 @@ function gcd(a,b) {
     }
 }
 
-exports.handler = (event, context, callback) => {
-    var instanceName = "lytnit" //event.instanceName;
-    var iterator = 12345; 		//event.iterator;
+// Take in these initial values from the requestor.
+var instanceName = "lytnit"		//event.instanceName;
+var iterator = 12345;			//event.iterator;
 
-    var c = Math.ceil(Math.log(iterator)/Math.log(36));
-    var p = Math.pow(36,c);
+// Let `c` be the number of characters required to map the iterator. 
+// Let `p` be the number of unique IDs obtainable by `c` characters.
+var c = Math.ceil(Math.log(iterator)/Math.log(36));
+var p = Math.pow(36,c);
 
-    var range = [Math.floor((0.28*p)), Math.floor(0.48*p-0.28*p)];    // [0:min, 1:delta]
-    var generator;
+// Define an ideal sample range for generator candidates. 
+var range = [Math.floor((0.28*p)), Math.floor(0.48*p-0.28*p)]; // [0:min, 1:delta]
 
-    // 
-    do { generator = range[0] + Math.floor(Math.random() * range[1]) }
-    while (gcd(generator,p)!=1);
+// Generate until valid. Don't hate me for this line. I needed the scope, but also wanted a 2-liner. lol
+var generator; do { generator = range[0] + Math.floor(Math.random() * range[1]) }
+while (gcd(generator,p)!=1);
 
-    console.log("GCD( " + generator + " , " + p + " ) = " + gcd(generator,p));
-
-    callback(null, generator);
-};
+// Log the results to the console for testing. >>> It should look like "GCD ( generator , p ) = 1".
+console.log("GCD( " + generator + " , " + p + " ) = " + gcd(generator,p));
