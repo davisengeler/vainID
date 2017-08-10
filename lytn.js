6034ae20dba79b2726b70fe8fbac0ff7c7bf42a6
Davis Engeler <davisengeler+github@gmail.com>
2017-08-10 00:10:13 -0500

    Add conditional for detecting outdated generators

--- lytn.js
+++ lytn.js
@@ -9,14 +9,20 @@ var newLink = "https://www.google.com/"; // Get this from parameters.
 // Grab instance metrics from DB
 var instanceName = "lytnit";
 // TODO: Connect to the DB to grab the instance's current `iterator` and `generator`.
-var localIterator = 15;   // We'll manipulate this value and save it to the DB later. Think of it as a local copy. 
+var localIterator = 14+1;   // We'll manipulate this value and save it to the DB later. Increment it here.  
 var generator = 13;  // This will hold the value from the DB.
 
-// The following is used to increase the capacity when the usage has exceeded the limits for the current number of digits. 
+// The following is used to select an updated generator after the ID length increases by 1 digit. 
 // Let `c` be the number of characters required to map the iterator. 
 // Let `p` be the number of unique IDs obtainable by `c` characters.
 var c = Math.ceil(Math.log(localIterator)/Math.log(36));
 var p = Math.pow(36,c);
+if (generator/p < 0.25) {
+    // TODO: Call function for the code found in `generator.js` to
+    // create and validate a new generator. Then save it to the 
+    // "instance data" in the DB. 
+    console.log("update generator");
+}
 
 // !TODO!: 
 //  saveLink(id, newLink): Returns false for collision. Returns true if saved successfully.
