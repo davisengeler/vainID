de3bf58eeab39a912fd1d69e914b0aa6f8f3a313
Davis Engeler <davisengeler+github@gmail.com>
2022-04-23 19:08:18 -0400

    Improve testing function

--- test.py
+++ test.py
@@ -1,4 +1,32 @@
-import vainID
+from __init__ import generate_id
 
-for i in range(1, 1000):
-    print(vainID.generate_id(i, "lytnit"))
+# Define test
+def run_test(seed, iterations=100):
+    used_ids = []
+    dupes = []
+    consecutive_dupes = 0
+    max_consecutive_dupes = 0
+    test_num = iterations
+
+    for i in range(0, test_num):
+        id = generate_id(i, seed)
+        if id in used_ids:
+            print(f"DUPLICATE: {id}")
+            print(f"Current atomic_iterator = {i}")
+            print(id)
+            dupes.append(id)
+            consecutive_dupes += 1
+        else:
+            print(f"No collision for id {id} at iterator = {i}")
+            if max_consecutive_dupes < consecutive_dupes:
+                max_consecutive_dupes = consecutive_dupes
+            consecutive_dupes = 0
+            used_ids.append(id)
+
+    print(f"Collision on {len(dupes)}/{test_num} ({(len(dupes)/test_num)*100}%)")
+    print(f"Max consecutive collisions: {max_consecutive_dupes}")
+
+# Run tests
+test_seed = "abc123"
+run_test(test_seed, 25000)
+# print(generate_id(14776336, test_seed))
